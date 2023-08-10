78e2838fa8e14066f484e38fefaebef5e99e448e
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2023-08-10 09:46:17 +0530

    My Admin Dashboard

---
+++ orders.js
@@ -0,0 +1,39 @@
+const Orders=[
+ 
+   {
+    productNumber:'Foldable Mini Drone',
+    productName:'85631',
+    paymentStatus:'Due',
+    shipping:'Pending'
+   },
+   {
+    productNumber:'CJI Mini Drone',
+    productName:'36632',
+    paymentStatus:'Refunded',
+    shipping:'Declined'
+   },
+   {
+    productNumber:'Realme 11 Pro Plus', 
+    productName:'49347',
+    paymentStatus:'Due',
+    shipping:'Pending'
+   },
+   {
+    productNumber:'Drone With Camera Drone',
+    productName:'99999',
+    paymentStatus:'Paid',
+    shipping:'Delivered'
+   },
+   {
+      productNumber:'GPS 4k Drone',
+      productName:'22821',
+      paymentStatus:'Paid',
+      shipping:'Delivered'
+   },
+   {
+      productNumber:'4K TV',
+      productName:'45215',
+      paymentStatus:'Due',
+      shipping:'Pending'
+   }
+]
